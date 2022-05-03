cc1c8a5208165df611b72103b206da4fe7df5e7d
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-05-03 02:05:12 -0300

    Tirando export default

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-function App() {
+import { Widget } from "./components/Widget";
 
+export function App() {
   return (
-    <div className="bg-violet-500 flex rounded ">
-      <h1 className="text-3xl font-bold p-2 hover:bg-violet-600 transition-colors">Hello world</h1>
-    </div>
+    <Widget/>
   )
 }
-
-export default App
