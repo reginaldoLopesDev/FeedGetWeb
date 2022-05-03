fd3c995b8e39a359c9eb8b9cb084777f2c576328
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-05-03 00:48:00 -0300

    Brincando com o tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
 
   return (
-    <div className="bg-violet-800">
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
+    <div className="bg-violet-500 flex rounded ">
+      <h1 className="text-3xl font-bold p-2 hover:bg-violet-600 transition-colors">Hello world</h1>
     </div>
   )
 }
