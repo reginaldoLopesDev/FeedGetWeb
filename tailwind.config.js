0c01f2a932b0f6f4b7fc3b563c8c5e4c53437efa
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-05-03 02:04:31 -0300

    Adicionando biblioteca de icone e definindo cor default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./src/**/*.tsx"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                brand: { 500: "#8257e6" },
+            },
+        },
     },
     plugins: [],
 };
