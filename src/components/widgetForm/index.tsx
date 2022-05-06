11e9f064e5c395e6ab43056b8e3670e7f75472cc
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-05-06 03:42:39 -0300

    Finalizando estilo no step de conteúdo

--- src/components/widgetForm/index.tsx
+++ src/components/widgetForm/index.tsx
@@ -6,6 +6,7 @@ import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
 import bugIconUrl from "../../assets/bug.svg"
 import ideaIconUrl from "../../assets/idea.svg"
 import thoughtIconUrl from "../../assets/thought.svg"
+import { FeedbackContentStep } from "./steps/FeedbackContentStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -33,17 +34,17 @@ export const feedbackTypes = {
 export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
 
-            <header>
-                <span className="text-xl leading-6">Deixe seu Feedback</span>
-                <CloseButton />
-            </header>
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>{feedbackType}</p>
+                <FeedbackContentStep feedbackType={feedbackType} onFeedbackRestartRequested={handleRestartFeedback} />
             )
             }
             <footer className="text-xs text-neutral-400">Feito com ❤️ por <a className="underline-offset-2" target="_blank" href="https://www.linkedin.com/in/reginaldosilvalopes/">Reginaldo Lopes</a></footer>
--- src/components/widgetForm/steps/FeedbackTypeStep.tsx
+++ src/components/widgetForm/steps/FeedbackTypeStep.tsx
@@ -1,24 +1,31 @@
 import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
 interface FeedbackTypeStepProps {
     onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
 
 export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
     return (
-        <div className="flex py-8 gap-2 w-full">
-            {Object.entries(feedbackTypes).map(([key, value]) => {
-                return (
-                    <button
-                        key={key}
-                        className="bg-zinc-800 py-5 w-24 items-center rounded-lg flex-1 flex flex-col gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                        type="button"
-                        onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-                    >
-                        <img src={value.image.src} alt={value.image.alt}></img>
-                        <span>{value.title}</span>
-                    </button>
-                );
-            })}
-        </div>
+        <>
+            <header>
+                <span className="text-xl leading-6">Deixe seu Feedback</span>
+                <CloseButton />
+            </header>
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            className="bg-zinc-800 py-5 w-24 items-center rounded-lg flex-1 flex flex-col gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            type="button"
+                            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+                        >
+                            <img src={value.image.src} alt={value.image.alt}></img>
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
+        </>
     );
 }
