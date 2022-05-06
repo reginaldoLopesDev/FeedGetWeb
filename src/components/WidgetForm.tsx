92ad3da1b74dea9b109b7a4465aa35d79408bc70
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-05-06 01:01:19 -0300

    Finalizando a primeira parte do form do widget

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CloseButton } from "./CloseButton";
 
 import bugIconUrl from "../assets/bug.svg"
@@ -5,7 +7,7 @@ import ideaIconUrl from "../assets/idea.svg"
 import thoughtIconUrl from "../assets/thought.svg"
 
 const feedbackTypes = {
-    BUG:{
+    BUG: {
         title: "Problema",
         image: {
             src: bugIconUrl,
@@ -15,49 +17,52 @@ const feedbackTypes = {
     IDEA: {
         title: "Ideia",
         image: {
-        src: ideaIconUrl,
-        alt: "Imagem de uma lâmpada"
+            src: ideaIconUrl,
+            alt: "Imagem de uma lâmpada"
         }
     },
     OTHER: {
         title: "Outro",
         image: {
-        src: thoughtIconUrl,
-        alt: "Imagem de uma nuvem de pensamento"
+            src: thoughtIconUrl,
+            alt: "Imagem de uma nuvem de pensamento"
         }
-
-
     }
 }
-
-export function WidgetForm(){
-    return(
+type FeedbackType = keyof typeof feedbackTypes;
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            
+
             <header>
                 <span className="text-xl leading-6">Deixe seu Feedback</span>
                 <CloseButton />
             </header>
-            <div className="flex py-8 gap-2 w-full">
-            {Object.entries(feedbackTypes).map(([key, value]) => {
-                return(
-                    <button
-                    key={key}
-                    className="bg-zinc-800 py-5 w-24 items-center rounded-lg flex-1 flex flex-col gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                    type="button"
-                    
-                    >
-                        <img src={value.image.src} alt={value.image.alt}></img>
-                        <span>{value.title}</span>
-                    </button>
-                );
-            })}
-            </div>
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
+                    {Object.entries(feedbackTypes).map(([key, value]) => {
+                        return (
+                            <button
+                                key={key}
+                                className="bg-zinc-800 py-5 w-24 items-center rounded-lg flex-1 flex flex-col gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                type="button"
+                                onClick={() => setFeedbackType(key as FeedbackType)}
+                            >
+                                <img src={value.image.src} alt={value.image.alt}></img>
+                                <span>{value.title}</span>
+                            </button>
+                        );
+                    })}
+                </div>
+            ) : (
+                <p>Conteúdo</p>
+            )}
             <footer className="text-xs text-neutral-400">Feito com ❤️ por <a className="underline-offset-2" target="_blank" href="https://www.linkedin.com/in/reginaldosilvalopes/">Reginaldo Lopes</a></footer>
-            
-            
-            
-            
-            </div>
+
+
+
+
+        </div>
     );
 }
